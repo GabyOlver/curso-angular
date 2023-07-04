2c9d078e56f81fa1f8551c6b3b7bef38087ac189
GabyOlver <127071903+GabyOlver@users.noreply.github.com>
2023-07-03 18:49:32 -0600

    componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'curso-angular';
 }
+  // Propiedad selector con valor app-root, este valor es el que usa para llamar al componente
+ 
+  //la propiedad templateUrl se asigna una direccion de un html cada que se llama a app-root se llama al doc html y desde ahi se muestra el contenido, 
+  //Todo el contenido se puede mostrar desde el archivo typescript para eso se usa unicamente template
+  //template permite definir el html directamente desde este componente por ejemplo:
+  
+  // template: `
+  // <h1> Curso de Angular </h1>
+  // `,
+ 
+//   styles: [`
+//   h1 {
+//     color: blue;
+//   }
+//  `]
+  // Ocurre lo mismo en styleUrls que en templateUrls
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { CounterComponent } from './counter/counter.component';
+
+//Definir todas las herramientas
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] //El componente appComponent forma parte de este modulo y esta es su herramienta principal
 })
 export class AppModule { }
